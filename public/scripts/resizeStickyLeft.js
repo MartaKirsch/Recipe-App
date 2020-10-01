2c77524cfbbd88d196d91176a7bbbb9c9269d615
MartaKirsch <martakirsch00@gmail.com>
2020-10-01 11:47:32 +0200

    added styles for 768px-992px

--- public/scripts/resizeStickyLeft.js
+++ public/scripts/resizeStickyLeft.js
@@ -7,13 +7,17 @@ const resize = (e) => {
   let wrapper =  document.querySelector('#wrapper');
   let padding = 2.5 * parseFloat(window.getComputedStyle(wrapper, null).getPropertyValue('padding-left'));
 
-  if(!leftWrapper.classList.contains('logIn'))
+  if(!leftWrapper.classList.contains('recipe')&&window.innerWidth<=1200)
   {
-    //set the stickyleft height so it's in the middle (top/bottom)
+    //set the stickyleft height so it's in the middle of the window (top/bottom)
     document.querySelector('#stickyLeft').style.height = (window.innerHeight - nav - padding)+'px';
   }
+  else
+  {
+    document.querySelector('#stickyLeft').style.height = 'auto';
+  }
 
-  if(document.body.offsetWidth<=1183 && !leftWrapper.classList.contains('logIn'))
+  if(window.innerWidth<=1200 && !leftWrapper.classList.contains('logIn'))
   {
     //set the left wrapper height so that it's the height of the whole document
     document.querySelector('#leftWrapper').style.height = (document.body.offsetHeight - nav - padding) + 'px';
